b29863a6a634ef3ce6e2d9cb4ec436cd09166587
DarshilGolaniya <darshil_22011@ldrp.ac.in>
2026-05-11 23:41:44 +0530

    Fixed database config

--- run.py
+++ run.py
@@ -9,10 +9,11 @@ app = create_app()
 if __name__ == "__main__":
 
     port = int(
-        os.environ.get("PORT", 5000)
+        os.environ.get("PORT", 10000)
     )
 
     app.run(
         host="0.0.0.0",
-        port=port
+        port=port,
+        debug=False
     )
