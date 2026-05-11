629f9059a36211464e036ae0d271f46a52a93030
DarshilGolaniya <darshil_22011@ldrp.ac.in>
2026-05-11 23:36:42 +0530

    Fixed database config

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
-from app import create_app, socketio
+import os
+
+from app import create_app
 
 
 app = create_app()
@@ -6,7 +8,11 @@ app = create_app()
 
 if __name__ == "__main__":
 
-    socketio.run(
-        app,
-        debug=True
+    port = int(
+        os.environ.get("PORT", 5000)
+    )
+
+    app.run(
+        host="0.0.0.0",
+        port=port
     )
